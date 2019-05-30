1453d1b043ecc9669bf17e2af50fedc24a3dfaca
Joshua Kobes <joshua.kobes@hotmail.com>
2019-05-30 11:40:13 +0200

    Added visual card representation

---
+++ js/buzzonline-screen.js
@@ -0,0 +1,16 @@
+
+/**
+ * BuzzOnline - The AirConsole Adaptation
+ *
+ * Copyright (c) Power of Interest, 2019
+ *
+ *
+ *
+ * Buzzonline Screen API for BuzzOnline, the card game
+ *
+ * See https://github.com/JoshuaK-PoI/buzz-online-airconsole
+ */
+
+
+
+
