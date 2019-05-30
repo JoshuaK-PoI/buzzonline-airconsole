eb92ecd39cf999e7f0da921cdd23ade8351d5f48
Joshua Kobes <joshua.kobes@hotmail.com>
2019-05-30 12:59:13 +0200

    Added Bootstrap source and dist (use NPM to build)

---
+++ js/buzzonline.js
@@ -0,0 +1,24 @@
+
+/**
+ * BuzzOnline - The AirConsole Adaptation
+ *
+ * Copyright (c) Power of Interest, 2019
+ *
+ *
+ *
+ * Buzzonline Screen API for BuzzOnline, the card game
+ *
+ * See https://github.com/JoshuaK-PoI/buzz-online-airconsole
+ */
+
+
+function BuzzOnline(opts) {
+    this.init_(opts);
+}
+
+
+
+
+BuzzOnline.prototype.init_ = function(opts) {
+
+}
